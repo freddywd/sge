e5dccca404c154c90ae1352e233110acbc2b732c
feippe <gabriel@feippe.com>
2019-03-13 12:07:12 -0300

    Add files via upload

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'portfolio.apps.PortfolioConfig',
+    'webpersonal.apps.Permisos',
+    'webpersonal.apps.GestionDeRecursos'
 ]
 
 MIDDLEWARE = [
